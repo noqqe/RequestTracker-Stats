78550e9bc75262dda88058fb37ca83d56ffeda17
Florian Baumann <flo@noqqe.de>
2011-04-14 22:14:24 +0200

    better statistics

---
+++ modules/most-active-creators-this-year.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+mysql --user="$stats_dbuser" --password="$stats_dbpassword" --batch -e \
+        "use \"$stats_database\" ;\
+        SELECT COUNT(DISTINCT EffectiveID) AS Tickets, Users.EmailAddress \
+        FROM Tickets, Users \
+        WHERE Users.id=Tickets.Creator \
+        AND Tickets.LastUpdated LIKE \"$(date +%Y)%\" \
+        GROUP BY Tickets.Creator \
+        ORDER BY Tickets DESC \
+        LIMIT 25; " | awk {'print $2":"$1'} | grep -v -e ^Email | $statistical_path
