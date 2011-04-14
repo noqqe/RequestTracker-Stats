78c6019120637df9e6770ffc0e1f41950076f662
Florian Baumann <flo@noqqe.de>
2011-04-14 20:32:58 +0200

    first alpha completly withou documentation^^

---
+++ modules/most-ticket-actions-this-month.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysql --user="$stats_dbuser" --password="$stats_dbpassword" --batch -e \
+        "use \"$stats_database\" ;\
+        SELECT Tickets.Status, COUNT(DISTINCT Tickets.EffectiveID) AS Tickets \
+        FROM Tickets \
+        WHERE LastUpdated LIKE \"$(date +%Y-%m)%\" \
+        GROUP BY Status \
+        ORDER BY Tickets DESC;" | awk {'print $1":"$2'} | grep -v -e ^Status | $statistical_path
