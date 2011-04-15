7041d87f91dbd77d71b79be4ccd71e2f8663f5b1
Florian Baumann <flo@noqqe.de>
2011-04-15 09:50:04 +0200

    working user activity plugin

--- modules/most-active-queue-this-year.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mysql --user="$stats_dbuser" --password="$stats_dbpassword" --batch -e \
-        "use \"$stats_database\" ;\
-        SELECT COUNT(DISTINCT Tickets.EffectiveID) AS Tickets, Queues.Name \
-        FROM Queues, Tickets \
-        WHERE Tickets.Queue=Queues.id \
-        AND Tickets.LastUpdated LIKE \"$(date +%Y)%\" \
-        GROUP BY Tickets.Queue \
-        ORDER BY Tickets DESC; " | awk {'print $2":"$1'} | grep -v -e ^Name | $statistical_path
