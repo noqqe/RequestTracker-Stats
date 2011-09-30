f532866b9cff2e87049e8f9b65fc2ad6214cd0c0
Florian Baumann <flo@noqqe.de>
2011-10-01 01:03:50 +0200

    Fixed date formatting problems. Thanks to cyco

--- modules/ticket-rate.bash
+++ modules/ticket-rate.bash
@@ -4,18 +4,20 @@
 # License: GPL-3 <http://www.gnu.org/licenses/gpl-3.0.txt>
 # Date: Tuesday 2011-04-12
 
-date_count=01
-while [ $date_count -le $(date +%m) ]; do
+date_count=1
+while [ $date_count -le $(date +"%-m") ]; do
+
+    if [ $date_count -le 9 ]; then
+        count=0$date_count
+    else
+   count=$date_count
+    fi
 
     mysql $stats_sqlopts -e "use \"$stats_database\" ; \
     SELECT COUNT(DISTINCT EffectiveID) AS Tickets \
     FROM Tickets \
-    WHERE Created LIKE \"$(date +%Y)-$date_count%\" " | awk "{print \"$date_count:\"\$1}" | grep -v Tickets 
+    WHERE Created LIKE \"$(date +%Y)-$count%\" " | awk "{print \"$date_count:\"\$1}" | grep -v Tickets 
 
     ((date_count++))
-
-    if [ $date_count -le 9 ]; then
-        date_count=0$date_count
-    fi
 
 done | $statistical_path
--- modules/user-activity.bash
+++ modules/user-activity.bash
@@ -8,25 +8,28 @@ for user in $(mysql $stats_sqlopts -e "use $stats_database; SELECT DISTINCT Owne
 
     if [ $(mysql $stats_sqlopts  -e "use $stats_database; SELECT COUNT(DISTINCT EffectiveID) AS Tickets FROM Tickets WHERE Tickets.Owner=$user AND Status=\"resolved\" AND LastUpdated LIKE \"$(date +%Y)%\" GROUP BY Tickets.Owner;" | grep -v ^Tickets | wc -l) -ge 1 ]; then  
 
-        date_count=01
+        date_count=1
      
         echo "$(mysql $stats_sqlopts -e "USE $stats_database; SELECT Name from Users WHERE id=$user" | grep -v ^Name ) "
         #echo "---------------------------------------------------"
     
-        while [ $date_count -le $(date +%m) ]; do
+         while [ $date_count -le $(date +"%-m") ]; do
+            if [ $date_count -le 9 ]; then
+                count=0$date_count
+            else
+                count=$date_count
+            fi
+
             mysql $stats_sqlopts -e "use $stats_database; \
             SELECT COUNT(DISTINCT EffectiveID) AS Tickets FROM Tickets, Users \
             WHERE Tickets.Owner=Users.id \
             AND Status=\"resolved\" \
-            AND Tickets.LastUpdated LIKE \"$(date +%Y)-$date_count%\" \
+            AND Tickets.LastUpdated LIKE \"$(date +%Y)-$count%\" \
             AND Tickets.Owner=\"$user\" \
-            GROUP BY Tickets.Owner;" | grep -v ^Tickets | awk "{print \"$date_count:\"\$1}" 
+            GROUP BY Tickets.Owner;" | grep -v ^Tickets | awk "{print \"$count:\"\$1}" | $statistical_path
 
             ((date_count++))
 
-            if [ $date_count -le 9 ]; then
-                date_count=0$date_count
-            fi 
         done | $statistical_path
 
         echo
